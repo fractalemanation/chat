0aaab25668184018b1f37163b9a6909e6974e079
Denis <warcraft0385@mail.ru>
2017-07-11 12:34:09 +0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+var lists = [];
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen (3000, function () {
+	console.log ('API app started');
+});
+
+io.on('connection', function (socket) {
+	console.log('connected new client');
+	socket.on('message', function (data) {
+		lists.push(data);
+		io.emit('new message', data);//Отправляем данные всем
+		//socket.broadcast.emit('new message', data);//Отправляем данные всем, кроме себя
+		//socket.id - уникальный индетификатор нового подключенного пользователя
+		//socket.join - подключить клиента к определенной комнате
+		//socket.leave - отключить клиента от определенной комнаты
+	});
+	socket.on('get', function (data) {
+		io.emit('new message', lists);
+	});
+	socket.on('clear', function (data) {
+		lists.splice(0,lists.length);
+	});
+});
