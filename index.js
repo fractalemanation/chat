ed5d4cfd927332881618a31171518db2440cc06c
Denis <warcraft0385@mail.ru>
2017-07-12 12:13:45 +0300

    init

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 var lists = [];
+var i = 1;
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -15,7 +16,7 @@ http.listen (process.env.PORT || 3000, function () {
 io.on('connection', function (socket) {
 	console.log('connected new client');
 	socket.on('message', function (data) {
-		lists.push(data);
+		lists.push('№' + i++ + ' - ' + data);
 		io.emit('new message', data);//Отправляем данные всем
 		//socket.broadcast.emit('new message', data);//Отправляем данные всем, кроме себя
 		//socket.id - уникальный индетификатор нового подключенного пользователя
@@ -23,9 +24,10 @@ io.on('connection', function (socket) {
 		//socket.leave - отключить клиента от определенной комнаты
 	});
 	socket.on('get', function (data) {
-		io.emit('new message', lists);
+		io.emit('gets', lists);
 	});
 	socket.on('clear', function (data) {
 		lists.splice(0,lists.length);
+		i = 1;
 	});
 });
